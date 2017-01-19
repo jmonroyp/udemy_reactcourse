4cd62d2129a21ca3108ab8ec8ed9339e5a5a3653
jmonroyp <jonathan.monroy@sw.com.mx>
2017-01-18 18:47:28 -0600

    - done

--- ReactWeather/app/components/Weather.jsx
+++ ReactWeather/app/components/Weather.jsx
@@ -6,31 +6,46 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
     getInitialState: function () {
         return {
-            location: 'Miami',
-            temp: 88
+            isLoading: false
         };
     },
     handleSearch: function (location) {
         var that = this;
+        this.setState({ isLoading: true });
+
+
         openWeatherMap.getTemp(location).then(
             function (temp) {
                 that.setState({
                     location: location,
-                    temp: temp
+                    temp: temp,
+                    isLoading: false
                 });
             },
             function (errorMessage) {
                 alert(errorMessage);
+                that.setState({ isLoading: false });
             }
         );
     },
     render: function () {
         var location = this.state.location;
         var temp = this.state.temp;
+        var {isLoading} = this.state;
+
+        function renderMessage() {
+            if (isLoading) {
+                return <h3>Fetching weather...</h3>;
+
+            } else if (temp && location) {
+                return <WeatherMessage location={location} temp={temp} />;
+            }
+        }
+
         return (
             <div>
                 <WeatherForm onSearch={this.handleSearch} />
-                <WeatherMessage location={location} temp={temp} />
+                {renderMessage()}
             </div>
         );
     }
