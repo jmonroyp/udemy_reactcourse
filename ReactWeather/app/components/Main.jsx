4287a503c3a97f074615a9e86cdc5c9cf1ace26e
jmonroyp <jonathan.monroy@sw.com.mx>
2017-01-23 18:41:29 -0600

    -- no se

--- ReactWeather/app/components/Main.jsx
+++ ReactWeather/app/components/Main.jsx
@@ -1,15 +1,24 @@
 var React = require('react');
 var Navigation = require('Navigation');
 
-var Main = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <Navigation></Navigation>
-                {this.props.children}
-            </div>
-        );
-    }
-});
+// var Main = React.createClass({
+//     render: function () {
+//         return (
+//             <div>
+//                 <Navigation></Navigation>
+//                 {this.props.children}
+//             </div>
+//         );
+//     }
+// });
+
+var Main = (props) => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            {props.children}
+        </div>
+    );
+};
 
 module.exports = Main;
