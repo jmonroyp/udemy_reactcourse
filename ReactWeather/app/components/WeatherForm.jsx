93a63a21766fda958a26016088707a9545ebe892
jmonroy <jonathan.monroy@unosquare.com>
2017-05-23 16:27:17 -0500

    - dne

--- ReactWeather/app/components/WeatherForm.jsx
+++ ReactWeather/app/components/WeatherForm.jsx
@@ -14,7 +14,7 @@ var WeatherForm = React.createClass({
 			<div>
 				<form onSubmit={this.onFormSubmit}>
 					<div>
-						<input type="text" ref="location" placeholder="Enter city name" />
+						<input type="search" ref="location" placeholder="Enter city name" />
 					</div>
 					<button className="button expanded hollow">Get Weather</button>
 				</form>
--- ReactWeather/app/components/WeatherMessage.jsx
+++ ReactWeather/app/components/WeatherMessage.jsx
@@ -1,16 +1,5 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-//     render: function () {
-//         var location = this.props.location;
-//         var temp = this.props.temp;
-//         return (
-//             <div>
-//                 <p>The temperature in {location} is {temp}</p>
-//             </div>
-//         );
-//     }
-// });
 
 var WeatherMessage = ({temp, location}) => {
     return (
